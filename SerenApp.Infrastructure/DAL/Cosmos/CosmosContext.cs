7d4591be9ebe0210392676e877d7bb26529940b0
Nicola Bovolato <nicola.bovolato@gmail.com>
2022-06-15 15:54:35 +0200

    Updated Cosmos context and DeviceData repository

--- SerenApp.Infrastructure/DAL/Cosmos/CosmosContext.cs
+++ SerenApp.Infrastructure/DAL/Cosmos/CosmosContext.cs
@@ -12,5 +12,6 @@ namespace SerenApp.Infrastructure.DAL.Cosmos
     {
         public DbSet<User> Users { get; set; }
         public DbSet<Device> Devices { get; set; }
+        public DbSet<DeviceData> DeviceData { get; set; }
     }
 }
---
+++ SerenApp.Infrastructure/DAL/Cosmos/DeviceDataRepository.cs
@@ -0,0 +1,55 @@
+using Microsoft.EntityFrameworkCore;
+using SerenApp.Core.Interfaces;
+using SerenApp.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerenApp.Infrastructure.DAL.Cosmos
+{
+    public class DeviceDataRepository : IDeviceDataRepository
+    {
+        private readonly CosmosContext context;
+        public DeviceDataRepository(CosmosContext context) {
+            this.context = context;
+        }
+
+        public async Task<DeviceData> Delete(DeviceData item)
+        {
+            context.Remove(item);
+            await context.SaveChangesAsync();
+            return item;
+        }
+
+        public async Task<IEnumerable<DeviceData>> GetAll()
+        {
+            return await context.DeviceData.ToListAsync();
+        }
+
+        public async Task<DeviceData> GetById(DateTime id)
+        {
+            return await context.DeviceData.FirstAsync(d => d.Id == id);
+        }
+
+        public async Task<IEnumerable<DeviceData>> GetManyByDevice(Device d)
+        {
+            return await context.DeviceData.Where(x => x.Device == d).ToListAsync();
+        }
+
+        public async Task<DeviceData> Insert(DeviceData item)
+        {
+            await context.AddAsync(item);
+            await context.SaveChangesAsync();
+            return item;
+        }
+
+        public async Task<DeviceData> Update(DeviceData item)
+        {
+            context.Update(item);
+            await context.SaveChangesAsync();
+            return item;
+        }
+    }
+}
