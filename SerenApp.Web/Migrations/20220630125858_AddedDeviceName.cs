61011f09ab5b656a89629a0b9c52b0e2427e263c
Nicola Bovolato <nicola.bovolato@gmail.com>
2022-06-30 15:01:54 +0200

    Added Device Name

---
+++ SerenApp.Web/Migrations/20220630125858_AddedDeviceName.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SerenApp.Web.Migrations
+{
+    public partial class AddedDeviceName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Devices",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Devices");
+        }
+    }
+}
