1dbfd6b5b94150299b84740f65b7ae7ac49e7cc2
Nicola Bovolato <nicola.bovolato@gmail.com>
2022-07-11 15:09:01 +0200

    Updated user graphs

--- SerenApp.Web/Pages/Device.razor.js
+++ SerenApp.Web/Pages/Device.razor.js
@@ -1,41 +1,100 @@
-export function RenderChart(chart, data) {
-    new Chart(
-        chart,
-        {
-            type: 'line',
-            data,
-            options: {
-                scales: {
-                    x: {
-                        type: 'time',
-                        display: true,
-                    },
-                    BodyTemperature: {
-                        type: 'linear',
-                        display: true,
-                    },
-                    BloodPressure: {
-                        type: 'linear',
-                        display: true,
-                    },
-                    BloodOxygen: {
-                        type: 'linear',
-                        display: true,
-                    },
-                    Battery: {
-                        type: 'linear',
-                        display: true,
-                    },
-                    HeartFrequency: {
-                        type: 'linear',
-                        display: true,
-                    },
-                    WalkCount: {
-                        type: 'linear',
-                        display: true,
-                    }
+const defaultChartOpts = {
+    type: 'line',
+    options: {
+        responsive: true,
+        interaction: {
+            mode: 'index',
+            intersect: false,
+        },
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+            decimation: {
+                enabled: true,
+            }
+        },
+        stacked: false,
+        scales: {
+            x: {
+                type: 'time',
+                time: {
+                    unit: 'hour'
+                }
+            },
+        }
+    }
+}
+
+export function RenderHeartChart(chart, data) {
+    const opts = {
+        ...defaultChartOpts,
+        data,
+        options: {
+            ...defaultChartOpts.options,
+            scales: {
+                ...defaultChartOpts.options.scales,
+                yBloodPressure: {
+                    type: 'linear',
+                    position: 'right',
+                    min: 50,
+                    max: 200
+                },
+                yHeartFrequency: {
+                    type: 'linear',
+                    position: 'left',
+                    min: 30,
+                    max: 220
                 }
             }
         }
-    );
+    }
+    new Chart(chart, opts);
+}
+
+export function RenderBodyChart(chart, data) {
+    const opts = {
+        ...defaultChartOpts,
+        data,
+        options: {
+            ...defaultChartOpts.options,
+            scales: {
+                ...defaultChartOpts.options.scales,
+                yBodyTemperature: {
+                    type: 'linear',
+                    position: 'right',
+                    min: 30,
+                    max: 50
+                },
+                yBloodOxygen: {
+                    type: 'linear',
+                    position: 'left',
+                    min: 60,
+                    max: 100
+                }
+            }
+        }
+    }
+    new Chart(chart, opts);
+}
+
+
+export function RenderBatteryChart(chart, data) {
+    const opts = {
+        ...defaultChartOpts,
+        data,
+        options: {
+            ...defaultChartOpts.options,
+            scales: {
+                ...defaultChartOpts.options.scales,
+                yBattery: {
+                    type: 'linear',
+                    position: 'left',
+                    min: 0,
+                    max: 100
+                }
+            }
+        }
+    }
+    new Chart(chart, opts);
 }
